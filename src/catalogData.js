b1eea9e4eebc1e688f12bae5fd0937d2e2fc2ab6
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2023-10-19 22:05:08 -0400

    Refactoring de test para verificar los títulos en catalogItems desde catalogData

--- src/catalogData.js
+++ src/catalogData.js
@@ -3,8 +3,17 @@ const catalogData = [
         Title: "CalcularMCD",
         Description: "Kata para calcular el Máximo Común Divisor (MCD) de dos números.",
         Difficulty: "Facil"
-
+    },
+    {
+        Title: "Ordenar Arreglos",
+        Description: "Kata para ordenar un arreglo de números en orden ascendente.",
+        Difficulty: "Intermedio"
+    },
+    {
+        Title: "Encontrar Ciclos en un Grafo",
+        Description: "Kata para encontrar ciclos en un grafo no dirigido.",
+        Difficulty: "Difícil"
     }
-    
 ];
+
 export default catalogData;
--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,16 @@
 import CatalogList from './index.js';
 import catalogData from './catalogData'; // Importa el arreglo de datos
 
-
 describe("Pruebas de obtención de título y descripción de la kata", () => {
-    it("Verifica si el campo 'Title' y 'description' del catálogo esta vacio", () => {
-      const catalogList = new CatalogList(catalogData); // Pasa el arreglo de datos al constructor
-  
-      const catalogItems = catalogList.displayCatalogNames();
-  
-      catalogItems.forEach(item => {
-        expect(item.title).toEqual("CalcularMCD");
-        expect(item.description).toEqual("Kata para calcular el Máximo Común Divisor (MCD) de dos números.");
-        expect(item.difficulty).toEqual("Facil")
-      });
+  it("Verifica si los títulos coinciden con los valores en catalogData", () => {
+    const catalogList = new CatalogList(catalogData); // Pasa el arreglo de datos al constructor
+
+    const catalogItems = catalogList.displayCatalogNames();
+
+    // Recorre todos los catálogos en catalogData
+    catalogData.forEach((catalog, index) => {
+      // Verifica que el título en catalogItems coincida con el título en catalogData
+      expect(catalogItems[index].title).toEqual(catalog.Title);
     });
   });
-  
+});
