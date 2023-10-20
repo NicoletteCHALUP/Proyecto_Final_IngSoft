0d8441b20a98850d43ca93b68155365aa3290e33
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2023-10-19 21:11:17 -0400

    Refactoring del test y retorna Titulo, Descripcion y dificultad del ejercicio de MCD

--- src/index.test.js
+++ src/index.test.js
@@ -1,30 +1,7 @@
 import CatalogList from './index.js';
 import catalogData from './catalogData'; // Importa el arreglo de datos
 
-describe("Pruebas de obtención de título de la kata", () => {
-  it("Verifica si el campo 'Title' del catálogo es correcto", () => {
-    const catalogList = new CatalogList(catalogData); // Pasa el arreglo de datos al constructor
 
-    const catalogItems = catalogList.displayCatalogNames();
-
-    catalogItems.forEach(item => {
-      expect(item.title).toEqual("CalcularMCD");
-    });
-  });
-});
-
-describe("Pruebas de obtención de título y descripción de la kata", () => {
-  it("Verifica si el campo 'Title' y 'description' del catálogo esta vacio", () => {
-    const catalogList = new CatalogList(catalogData); // Pasa el arreglo de datos al constructor
-
-    const catalogItems = catalogList.displayCatalogNames();
-
-    catalogItems.forEach(item => {
-      expect(item.title).toEqual("CalcularMCD");
-      expect(item.description).toEqual("Kata para calcular el Máximo Común Divisor (MCD) de dos números.");
-    });
-  });
-});
 describe("Pruebas de obtención de título y descripción de la kata", () => {
     it("Verifica si el campo 'Title' y 'description' del catálogo esta vacio", () => {
       const catalogList = new CatalogList(catalogData); // Pasa el arreglo de datos al constructor
@@ -33,9 +10,8 @@ describe("Pruebas de obtención de título y descripción de la kata", () => {
   
       catalogItems.forEach(item => {
         expect(item.title).toEqual("CalcularMCD");
+        expect(item.description).toEqual("Kata para calcular el Máximo Común Divisor (MCD) de dos números.");
         expect(item.difficulty).toEqual("Facil")
-
-        
       });
     });
   });
